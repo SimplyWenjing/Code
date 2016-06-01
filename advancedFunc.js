3fed6c787f5e0412dc4615ea3667d83f07303ded
wenjinggu <598228662@qq.com>
2016-06-01 10:51:07 +0800

    高级函数
    数组分块，安全类型检测

---
+++ advancedFunc.js
@@ -0,0 +1,29 @@
+/*============================安全的类型检测===================================*/
+//检测是否为数组
+function isArray (value) {
+    return Object.prototype.toString.call(value) == "[object Array]";
+}
+//检测是否为函数
+function isFunction (value) {
+    return Object.prototype.toString.call(value) == "[object Function]";
+}
+//检测是否为正则表达式
+function isRegExp (value) {
+    return Object.prototype.toString.call(value) == "[object RegExp]";
+}
+/*===============================实现数组分块================================*/
+function arrayChunk (array,process,context,delay) {
+	if(delay) {
+		interval = delay;
+	} else {
+		interval = 100;
+	}
+	setTimeout(function () {
+		var item = array.shift();//取出下一个条目
+		process.call(context,item);//处理下一个条目
+
+		if (array.length > 0) {//若还有条目，则设置另一个定时器
+			setTimeout(arguments.callee,interval);
+		}
+	},interval);
+}
