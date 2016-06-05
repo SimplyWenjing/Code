bfd1026380ef0d46aac609c33dde35017418786b
wenjinggu <598228662@qq.com>
2016-06-05 17:25:10 +0800

    修改

--- cookies.js
+++ cookies.js
@@ -1,32 +1,5 @@
-//cookie 的读写
-// var CookieUtil = {
-// 	get: function (name) {
-// 	//获取cookie
-// 		var cookieName = encodeURIComponent(name) + "=";
-// 		var cookieStart = document.cookie.indexOf(cookieName);
-// 		var cookieValue = null;
-// 		if (cookieStart > -1) {
-// 			var cookieEnd = document.cookie.indexOf(";",cookieStart);
-// 			if (cookieEnd == -1) {
-// 				cookieEnd = document.cookie.length; 
-// 			}
-// 			cookieValue = decodeURIComponent(document.cookie.substring(cookieStart+cookieName.length,cookieEnd));
-// 		}
-// 		return cookieValue;
-// 	},
-// 	set: function (name,value) {
-// 		//设置cookie
-// 		var cookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-// 		document.cookie = cookieText;
-// 	},
-// 	unset: function (name) {
-// 		//取消cookie
-// 		this.set(name,"");
-// 	}
-
-// }
 var CookieUtil = {
-
+	
     get: function (name){
         var cookieName = encodeURIComponent(name) + "=",
             cookieStart = document.cookie.indexOf(cookieName),
