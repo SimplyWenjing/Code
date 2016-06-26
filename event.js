3cf500d8a93ee61767a3113501df68e030e502b9
wenjinggu <598228662@qq.com>
2016-06-26 15:58:43 +0800

    跨浏览器的事件对象
    getEvent/getTarget/stopPropagation/preventDefault/

--- event.js
+++ event.js
@@ -1,4 +1,5 @@
 var EventUtil = {
+	//添加事件处理程序
 	addHandler: function (element,type,handler) {
 		if (element.addEventListener) {
 			element.addEventListener(type,handler,false);
@@ -8,6 +9,7 @@ var EventUtil = {
 			element["on" + tyle] = handler;
 		}
 	},
+	//取消事件处理程序
 	removeHandler: function (element,type,handler) {
 		if (element.removeEventListener) {
 			element.removeEventListener(type,handler,false);
@@ -16,5 +18,29 @@ var EventUtil = {
 		} else {
 			element["on" + tyle] = null;
 		}
+	},
+	//获取事件对象
+	getEvent: function (event) {
+		return event ? event : window.event;
+	},
+	//获取事件目标对象
+	getTarget: function (event) {
+		return event.target || event.srcElement;
+	},
+	//阻止默认行为
+	preventDefault: function (event) {
+		if (event.preventDefault) {
+			event.preventDefault();
+		} else {
+			event.returnValue = false;//取消默认行为
+		}
+	},
+	//停止事件冒泡
+	stopPropagation: function (event) {
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		} else {
+			event.cancleBubble = true;
+		}
 	}
-}
+};
