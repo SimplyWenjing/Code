cbcd9ebeebbb7a167be7ea0d73eec1f038bd5786
wenjing <598228662@qq.com>
2016-08-07 20:42:29 +0800

    快速排序

--- sort.js
+++ sort.js
@@ -6,18 +6,38 @@ var sort = {
 			return a-b;
 		});
 	},
+	//冒泡排序，依次比较前后两个，大的放在后面，时间复杂度 n的平方
 	bubbleSort: function (array) {
 		var len = array.length;
 		var i,j,d;
 		for(i = 0; i < len; i ++) {
-			for(j = i+1; j < len; j ++) {
-				if (array[i] > array[j]) {
+			for(j = 1; j < len-i; j ++) {
+				if (array[j-1] > array[j]) {
 					d = array[j];
-					array[j] = array[i];
-					array[i] = d;
+					array[j] = array[j-1];
+					array[j-1] = d;
 				}
 			}
 		}
 		return array;
+	},
+	//快速排序
+	quickSort: function (array) {
+		if (len <= 1) {
+			return array;
+		}
+		var pivot    = array[0];
+		var len      = array.length;
+		var leftArr  = [];
+		var rightArr = [];
+		for(var i = 1; i < len; i ++) {
+			if (array[i] < pivot) {
+				leftArr.push(array[i]);
+			} else {
+				rightArr.push(array[i]);
+			}
+		}
+		return arguments.callee(leftArr).concat(pivot,arguments.callee(rightArr));
 	}
+
 }
